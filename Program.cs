3a864c483d36332c23ca222181aef097d95249be
flip701 <paulshipspb@gmail.com>
2023-04-26 15:24:21 +0300

    Решение второй  задачи

--- Program.cs
+++ Program.cs
@@ -15,4 +15,33 @@ for (int i = 1; i <= m; i++)
     }
 }
 
-Console.WriteLine($"Количество положительных чисел: {count}");
+// Console.WriteLine($"Количество положительных чисел: {count}");
+///////////////////////////////////////////////////////////////////
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+using System;
+
+        double k1, b1, k2, b2, x, y;
+
+        Console.WriteLine("Введите значения для первой прямой:");
+        Console.Write("k1 = ");
+        k1 = double.Parse(Console.ReadLine());
+        Console.Write("b1 = ");
+        b1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значения для второй прямой:");
+        Console.Write("k2 = ");
+        k2 = double.Parse(Console.ReadLine());
+        Console.Write("b2 = ");
+        b2 = double.Parse(Console.ReadLine());
+
+        if (k1 == k2) {
+            Console.WriteLine("Прямые параллельны");
+        } else {
+            x = (b2 - b1) / (k1 - k2);
+            y = k1 * x + b1;
+            Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
+        }
+
+        
